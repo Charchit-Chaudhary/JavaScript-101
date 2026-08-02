b6b2818fbeb6f23c5ce35461585e3a12d713d4e6
Charchit-Chaudharygit config --global user.email charchit12356@gmail.com <charchit12356@gmail.com>
2026-08-02 09:13:20 +0545

    JS Internals

--- Javascript Internals.js
+++ Javascript Internals.js
@@ -33,3 +33,35 @@ function Outer(){
 let Morty=Outer();
 console.log(Morty);// here we see when we call the outer function it returns inner function
 Morty()// here it executes that inner function+outer enviornment hidden link(name) which called closures
+
+
+
+
+function spain(){
+    let defeat="france"
+    function fifa(){
+        console.log(`Spain knocked out ${defeat}.`);
+
+    }
+    return fifa;
+}
+let final=spain();
+ final();
+
+
+ //Hoisting
+
+console.log(num);
+// console.log(age);
+Hello();
+// Hoisting is Js behaviour that hoist varaible and function declarations on top of their scope but we actually think js moves the varaible or function declarations up above but what js does before that we know that everytime js enters scope and it run two phases: 
+// 1] Creation Phase: In this first phase before executing any line of code JS scans the file and allocates the memory space for variables and functions(Only decealrations not assignment)
+// 2] Execution Phase: In this JS starts executing the code line by line from top to bottom.
+
+var num=10;// In var, hoisting also works means that it can hoist var decelaration only, not assignment.
+
+let age=22;// But in let/const we can see that it throws refrence error, because we cannot access age before initialization, this happens because unlike var let/const respect their scope and they cannot be initalized until their decleration line execute and they locked in TDZ.
+
+function Hello(){
+    console.log("Merci");// Here we can see that the function declaration is hoisted along with its body because, during the creation phase, JavaScript creates the function object and stores it in memory.
+}
